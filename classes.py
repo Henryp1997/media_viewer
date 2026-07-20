a5039b0856e23ddd50aad7205cf185b380e2a2f8
Henry Pickersgill <henry.pickersgill@forefrontrf.com>
2026-07-20 14:29:23 +0100

    move classes to classes.py

---
+++ classes.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+class ArrowKeyState():
+    """ Class for holding arrow key pressed state """
+    left: bool = False
+    right: bool = False
+    up: bool = False
+    down: bool = False
+
+
+@dataclass
+class PanelConfig():
+    """ Configuration for application panels and borders """
+    banner_height: float # Top panel height
+    border_width: float  # Right edge width
+    panel_width: float   # Left navigation panel width
+
+
+@dataclass
+class ViewportConfig():
+    padding: float
+    n_btns_per_row: float
+    btn_separation: tuple # (horizontal, vertical) space between button edges in pixels
--- main.py
+++ main.py
@@ -4,32 +4,10 @@ from dataclasses import dataclass
 import pygame as pg
 from Artist import Artist
 from Button import Button
+from classes import ArrowKeyState, ViewportConfig, PanelConfig
 
 os.environ["SDL_VIDEO_WINDOW_POS"] = "0,0"
 pg.init()
-
-
-class ArrowKeyState():
-    """ Class for holding arrow key pressed state """
-    left: bool = False
-    right: bool = False
-    up: bool = False
-    down: bool = False
-
-
-@dataclass
-class PanelConfig():
-    """ Configuration for application panels and borders """
-    banner_height: float # Top panel height
-    border_width: float  # Right edge width
-    panel_width: float   # Left navigation panel width
-
-
-@dataclass
-class ViewportConfig():
-    padding: float
-    n_btns_per_row: float
-    btn_separation: tuple # (horizontal, vertical) space between button edges in pixels
 
 
 class MediaViewer():
