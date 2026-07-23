1d79a19001f57f7f04eafa4a115aa35a646bb2e5
Henry Pickersgill <henry.pickersgill@forefrontrf.com>
2026-07-23 16:06:17 +0100

    add missing Navbar.py in last commits

---
+++ Navbar.py
@@ -0,0 +1,50 @@
+"""
+Navbar.py
+
+Defines a Navbar class to hold Buttons and allow the user
+to navigate between types of media and to settings
+
+    - Author: HP (2026)
+"""
+from Artist import Artist
+from classes import NavbarConfig, BorderConfig
+from Button import Button
+
+
+class Navbar():
+    def __init__(
+        self,
+        artist: Artist,
+        navbar_config: NavbarConfig,
+        border_config: BorderConfig
+    ) -> None:
+        self.artist = artist
+        self.cfg = navbar_config
+        self.border_cfg = border_config
+        self.create_buttons()
+
+
+    def draw(self):
+        """ Draw the Navbar and its buttons """
+        self.artist.draw_rect(
+            color=self.cfg.bg_color, x=0, y=0,
+            w=self.cfg.navbar_width, h=self.artist.display.SCREEN_Y
+        )
+        for btn in self.buttons:
+            btn.draw()
+
+    
+    def create_buttons(self):
+        """ Create all Navbar buttons """
+        pad = self.cfg.padding
+        width = self.cfg.navbar_width - 2 * pad
+        height = width
+        self.buttons = [
+            Button(
+                self.artist, text="x",
+                x=pad,
+                y=self.border_cfg.banner_height + pad,
+                w=width, h=height,
+                border_radius=8, bg_colour="#00FF00"
+            )
+        ]
